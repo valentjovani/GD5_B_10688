53f4040594e5b73472356ca9b2bcb506d1bc7bc6
valentjovani <valentjovani@gmail.com>
2022-09-21 20:22:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gdroom_b_10688/room/Note.kt
@@ -0,0 +1,12 @@
+package com.example.gdroom_b_10688.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note: String
+)
---
+++ app/src/main/java/com/example/gdroom_b_10688/room/NoteAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.gdroom_b_10688.room
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.viewmodel.savedstate.R
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.adapter_note.view.*
+
+class NoteAdapter (private val notes: ArrayList<Note>, private val
+listener: OnAdapterListener) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            NoteViewHolder {
+        return NoteViewHolder(
+
+            LayoutInflater.from(parent.context).inflate(com.example.gdroom_b_10688.R.layout.adapter_note,parent, false)
+        )
+    }
+    override fun onBindViewHolder(holder: NoteViewHolder, position:
+    Int) {
+        val note = notes[position]
+        holder.view.text_title.text = note.title
+        holder.view.text_title.setOnClickListener{
+            listener.onClick(note)
+        }
+        holder.view.icon_edit.setOnClickListener {
+            listener.onUpdate(note)
+        }
+        holder.view.icon_delete.setOnClickListener {
+            listener.onDelete(note)
+        }
+    }
+    override fun getItemCount() = notes.size
+    inner class NoteViewHolder( val view: View) :
+        RecyclerView.ViewHolder(view)
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(list: List<Note>){
+        notes.clear()
+        notes.addAll(list)
+        notifyDataSetChanged()
+    }
+    interface OnAdapterListener {
+        fun onClick(note: Note)
+        fun onUpdate(note: Note)
+        fun onDelete(note: Note)
+    }
+}
